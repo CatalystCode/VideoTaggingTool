1dad32e39b851fde93915d3dc459d202330ba8f4
amiturgman <ami.turgman@microsoft.com>
2016-01-01 11:28:09 +0200

    trying bower again 2

--- deploy.cmd
+++ deploy.cmd
@@ -106,12 +106,10 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 )
 
 :: 4. Install bower packages
-IF EXIST "%DEPLOYMENT_TARGET%\public\bower.json" (
-  pushd "%DEPLOYMENT_TARGET%\public"
-  call :ExecuteCmd ..\node_modules\.bin\bower install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
+pushd "%DEPLOYMENT_TARGET%\public"
+call :ExecuteCmd "!NODE_EXE!" ..\node_modules\bower\bin\bower install
+IF !ERRORLEVEL! NEQ 0 goto error
+popd
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
