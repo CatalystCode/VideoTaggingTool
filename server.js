cd9aa80de89bcc4b1fab98409ec7c9146c89014c
amiturgman <ami.turgman@microsoft.com>
2016-01-04 10:27:11 +0200

    changing to cloud settings

--- server.js
+++ server.js
@@ -70,8 +70,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-//app.set('port', process.env.PORT || 3000);
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
