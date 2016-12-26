19ce416216c1e3b81f0b09077097e0f84455d6ff
aribornstein <aribornstein@gmail.com>
2016-12-26 16:21:23 +0200

    Updated ensure schema path

--- storage/sql/ensureSchema.js
+++ storage/sql/ensureSchema.js
@@ -30,7 +30,7 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_SERVER, !pro
     
     // Construct the database according to the updated schema.sql
     var createdb_command = 'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -i ' + 
-      path.join('deployment', 'sql', 'schema.sql') + ' > createdb.log';
+      path.join('storage', 'sql', 'schema.sql') + ' > createdb.log';
     child_process.exec(createdb_command, null, function (error, stdout, stderr) {
       
       if (error) return console.error('Error creating new DB schema', error);
@@ -40,7 +40,7 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_SERVER, !pro
       var insertAdmin =  'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -Q ' + '"INSERT INTO [dbo].[Users] ([Name] ,[Email] ,[RoleId]) VALUES (\'User Name\' ,\'%DB_EMAIL%\' ,2)"';
       child_process.exec(createdb_command, null, function (error, stdout, stderr) {
       
-        if (error) return console.error('Error creating inserting admin', error);
+        if (error) return console.error('Error inserting admin', error);
         return console.info('DB schema deployed successfully');
       });
 
