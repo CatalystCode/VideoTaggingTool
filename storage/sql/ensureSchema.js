51aa600ec746e8a011348db5268251dc88e464ff
aribornstein <aribornstein@gmail.com>
2016-12-27 09:49:26 +0200

    cleaned deployment script

--- storage/sql/ensureSchema.js
+++ storage/sql/ensureSchema.js
@@ -37,7 +37,7 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_SERVER, !pro
 
 
       //insert admin user
-      var insertAdmin =  'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -Q ' + '"INSERT INTO [dbo].[Users] ([Name] ,[Email] ,[RoleId]) VALUES (\'Admin\' ,\'%DB_EMAIL%\' ,2)"';
+      var insertAdmin =  'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -Q ' + '"INSERT INTO [dbo].[Users] ([Name] ,[Email] ,[RoleId]) VALUES (\'%DB_USER%\' ,\'%DB_EMAIL%\' ,2)"';
       child_process.exec(insertAdmin, null, function (error, stdout, stderr) {
       
         if (error) return console.error('Error inserting admin', error);
